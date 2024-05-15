b2afa7b2de242e5278d3b356a2d144dadf64c46f
ashiqur-shohan <ashiqur.shohan@gmail.com>
2024-05-15 21:10:50 +0600

    comment

--- abstraction.cpp
+++ abstraction.cpp
@@ -1,1 +1,40 @@
 // Abstraction means displaying only essential information and hiding the details
+// abstraction is two type
+    // a. Data abstraction
+    // b. Control Abstraction
+#include<bits/stdc++.h>
+
+using namespace std;
+class Account{
+    // Access Specifier
+    private:
+        int amount;
+        string acc_name;
+
+    // Access Specifier
+    public:
+    void deposite(int tk){
+        amount = tk + amount;
+        cout<<"Your Balance"<<amount<<endl;
+    }
+    void withdraw(int tk,string){
+        if (tk < amount){
+            amount = amount - tk;
+            cout<<"Your Balance"<<amount<<endl;
+        }
+        else {
+            cout<<'you do not have enough money';
+        }
+    }
+    void change_acc_name(string name){
+        acc_name = name;
+        cout<<acc_name<<endl;
+    }
+};
+
+int main (){
+    Account acc1 = Account();
+    acc1.deposite(500);
+    
+    return 0;
+}
